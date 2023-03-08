d282a192e96f6c411cfeef01e7013dd932ba6737
yousaf <yousafbinfarrukh>
2023-03-08 21:26:03 +0500

    Created Seperated Folder for Arduino

---
+++ main/handleLedMode.ino
@@ -0,0 +1,56 @@
+void handleLedMode()
+{
+  switch (led_mode)
+  {
+  case MODE_RAINBOW:
+    rainbow(WAIT_TIME);
+    break;
+  case MODE_RAINBOW_CYCLE:
+    rainbowCycle(WAIT_TIME);
+    break;
+
+  case MODE_RAINBOW_THEATRE:
+     theaterChaseRainbow(WAIT_TIME);
+     break;
+
+  case MODE_THEATRE_WHITE:
+    theaterChase(strip.Color(255, 255, 255), WAIT_TIME);
+    break;
+
+  case MODE_THEATRE_RED:
+    theaterChase(strip.Color(255,   0,   0), WAIT_TIME);
+    break;
+
+  case MODE_THEATRE_GREEN:
+    theaterChase(strip.Color(0,   255,   0), WAIT_TIME);
+    break;
+
+  case MODE_THEATRE_BLUE:
+    theaterChase(strip.Color(  0,   0, 255), WAIT_TIME);
+    break;
+
+  case MODE_RED:
+    colorWipe(strip.Color(255, 0, 0), WAIT_TIME);
+    break;
+
+  case MODE_GREEN:
+    colorWipe(strip.Color(0, 255, 0), WAIT_TIME);
+    break;
+
+  case MODE_BLUE:
+    colorWipe(strip.Color(0, 0, 255), WAIT_TIME);
+    break;
+
+  case MODE_CUSTOM:
+    colorWipe(strip.Color(r, g, b), WAIT_TIME);
+    break;
+
+  case MODE_TURNOFF:
+    colorWipe(strip.Color(0, 0, 0), WAIT_TIME);
+    break;
+
+  default:
+    rainbow(20);
+    break;
+  }
+}
---
+++ main/rainbow.ino
@@ -0,0 +1,11 @@
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
